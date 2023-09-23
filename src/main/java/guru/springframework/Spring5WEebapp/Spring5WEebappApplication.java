42ae708d23fe90104e1d48be8fda513bc366be8c
Ashok <yarrabatiashok411@gmail.com>
2023-09-23 18:25:13 +0530

    Adde new files

---
+++ src/main/java/guru/springframework/Spring5WEebapp/Spring5WEebappApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.Spring5WEebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5WEebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5WEebappApplication.class, args);
+	}
+
+}
